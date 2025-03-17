0525a93e7859e9e61072efb2f4d774dba70d915f
Murillo Sergio <murillosergionsousa@outlook.com>
2025-03-17 12:47:31 -0300

    Beta 0.1

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# Aquae Attractio Calculator - 2025
+# Open Source
+
+
+nome = input("Qual é seu nome? ")
+
+print(f"Olá, {nome} ")
+atividade_fisica = input(f"{nome} você se considera: A - uma pessoa físicamente ativa ou B -uma pessoa sedentária? ")
+peso = float(input(f"Ok{nome}, Quanto você pesa atualmente? "))
+if "A":
+    print(f"Ok {nome}, você se considera uma pessoa fisicamente ativa!")
+    print({peso})
+elif "B":
+    print(f"Ok {nome}, você se considera uma pessoa sedentária!")
+    print({peso})
+
+
+
+
+
