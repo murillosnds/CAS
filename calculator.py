aaf746ea7152ea9ec9bdd84325170384f8a98bf3
Murillo Sergio <murillosergionsousa@outlook.com>
2025-03-17 13:02:34 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,15 +6,15 @@ nome = input("Qual é seu nome? ")
 
 print(f"Olá, {nome} ")
 atividade_fisica = input(f"{nome} você se considera: A - uma pessoa físicamente ativa ou B -uma pessoa sedentária? ")
-peso = float(input(f"Ok{nome}, Quanto você pesa atualmente? "))
 if "A":
     print(f"Ok {nome}, você se considera uma pessoa fisicamente ativa!")
-    print({peso})
 elif "B":
     print(f"Ok {nome}, você se considera uma pessoa sedentária!")
-    print({peso})
 
-
-
-
-
+peso = float(input(f"Ok {nome}, Quanto você pesa atualmente (em kg)? "))
+print(f"Você pesa {peso}kg, {nome}? Responda SIM ou NÃO ")
+if "SIM":
+    print("Ok!")
+elif "NÂO":
+    print("Por favor, insira seu peso.")
+    print("Escreva V para voltar!")
