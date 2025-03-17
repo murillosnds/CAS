2dd4a3727929c4bace1099a130b1441a349e8afa
Murillo Sergio <murillosergionsousa@outlook.com>
2025-03-17 15:41:35 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,36 @@
 # Aquae Attractio Calculator - 2025
 # Open Source
+# By @murillosnds on GitHub 
 
 
 nome = input("Qual é seu nome? ")
 
 print(f"Olá, {nome} ")
-atividade_fisica = input(f"{nome} você se considera: A - uma pessoa físicamente ativa ou B -uma pessoa sedentária? ")
-if "A":
+atividade_fisica = input(f"{nome} você se considera: A - uma pessoa físicamente ativa ou B -uma pessoa sedentária? | Resposta: ")
+if atividade_fisica == "A":
     print(f"Ok {nome}, você se considera uma pessoa fisicamente ativa!")
-elif "B":
+elif atividade_fisica == "B":
     print(f"Ok {nome}, você se considera uma pessoa sedentária!")
 
 peso = float(input(f"Ok {nome}, Quanto você pesa atualmente (em kg)? "))
-print(f"Você pesa {peso}kg, {nome}? Responda SIM ou NÃO ")
-if "SIM":
+confirmação_peso = input(f"Você pesa {peso:.0f}kg, {nome}? Responda SIM ou NÃO | Resposta: ")
+if confirmação_peso == "SIM":
     print("Ok!")
-elif "NÂO":
-    print("Por favor, insira seu peso.")
-    print("Escreva V para voltar!")
+elif confirmação_peso == "NÃO":
+    float(input("Por favor, insira seu peso (em kg) | Resposta: "))
+    print("Ok!")
+else:
+    print("ERRO CRITICO! Reinicie o programa.")
+
+peso_ativo = peso * 50
+peso_sedentario = peso * 35 
+
+if atividade_fisica == "A":
+    print(f"Você deve ingerir {peso_ativo:.0f}ml ou {peso_ativo/1000}litros por dia")
+elif atividade_fisica == "B":
+    print(f"Você deve ingerir {peso_sedentario:.0f}ml ou {peso_sedentario/1000}litros por dia")
+
+
+
+
+
