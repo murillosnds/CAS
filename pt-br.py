283889558bb0a48a5c2ec3be459d6ff194975777
Murillo Sergio <murillosergionsousa@outlook.com>
2025-03-18 19:52:47 -0300

    Update pt-br.py

--- pt-br.py
+++ pt-br.py
@@ -12,6 +12,19 @@ if atividade_fisica == "A":
 elif atividade_fisica == "B":
     print(f"Ok {nome}, você se considera uma pessoa sedentária!")
 
+idade = int(input("Qual a sua idade? | Resposta: "))
+
+if idade >= 66:
+    print("Vamos usar 25 ml por cada kg")
+elif idade >= 56:
+    print("Vamos usar 30 ml por cada kg")
+elif idade >= 18:
+    print("Vamos usar 35 ml por cada kg")
+else:
+    print("Vamos usar 40 ml por cada kg")
+
+
+
 peso = float(input(f"Ok {nome}, Quanto você pesa atualmente (em kg)? "))
 confirmação_peso = input(f"Você pesa {peso:.0f}kg, {nome}? Responda SIM ou NÃO | Resposta: ")
 if confirmação_peso == "SIM":
@@ -22,15 +35,27 @@ elif confirmação_peso == "NÃO":
 else:
     print("ERRO CRITICO! Reinicie o programa.")
 
-peso_ativo = peso * 50
-peso_sedentario = peso * 35 
+peso_jovem = peso * 40
+peso_jovem_adulto = peso * 35
+peso_adulto_velho = peso * 30
+demais_idade = peso * 25
 
-if atividade_fisica == "A":
-    print(f"Você deve ingerir {peso_ativo:.0f}ml ou {peso_ativo/1000}litros por dia")
-elif atividade_fisica == "B":
-    print(f"Você deve ingerir {peso_sedentario:.0f}ml ou {peso_sedentario/1000}litros por dia")
+if atividade_fisica == "A" and idade < 18:
+    print(f"Você deve ingerir {peso_jovem:.0f}ml ou {peso_jovem/1000}litros por dia")
 
+elif atividade_fisica == "B" and idade < 18:
+    print(f"Você deve ingerir {peso_jovem:.0f}ml ou {peso_jovem/1000}litros por dia")
 
+elif atividade_fisica == "A" and idade >= 18 and 55:
+    print(f"Você deve ingerir {peso_jovem_adulto:.0f}ml ou {peso_jovem_adulto/1000}litros por dia")
 
+elif atividade_fisica == "B" and idade >= 18 and 55:
+    print(f"Você deve ingerir {peso_jovem_adulto:.0f}ml ou {peso_jovem_adulto/1000}litros por dia")
 
+elif atividade_fisica == "A" and idade >= 56 and 65:
+    print(f"Você deve ingerir {peso_adulto_velho:.0f}ml ou {peso_adulto_velho/1000}litros por dia")
 
+elif atividade_fisica == "B" and idade >= 56 and 65:
+    print(f"Você deve ingerir {peso_adulto_velho:.0f}ml ou {peso_adulto_velho/1000}litros por dia")
+else:
+    print(f"Você deve ingerir {demais_idade:.0f}ml ou {demais_idade/1000}litros por dia")
