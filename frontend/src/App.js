492459ff42f0e309bb8c9c3d597e56cdf1f11144
Murillo Sergio <murillosnds@gmail.com>
2025-09-20 19:22:27 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -76,7 +76,7 @@ function App() {
 
   const calcularAgua = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/calcular', {
+      const response = await fetch('https://cas-hygy.onrender.com/calcular', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ peso, idade, atividade, clima })
